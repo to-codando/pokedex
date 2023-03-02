f0327b42532f052f2a2b47e775cad00a92c655b1
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2023-03-02 13:40:23 -0300

    feat: create basic AppMain

--- src/components/appMain/index.ts
+++ src/components/appMain/index.ts
@@ -1,11 +1,20 @@
 import { html, css } from "iares";
 
 const styles = () => css`
-  app-main { }
+  app-main {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-wrap:wrap;
+    width:100%;
+    height: 100vh;
+  }
 `;
 
 const template = () => html`
-  <h1>App main</h1>
+  <div class="wrap">
+
+  </div>
 `;
 
 export const AppMain = () => {
