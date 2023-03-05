f8f95489fd2321fb9afda77f646adca0c58be152
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2023-03-05 18:18:16 -0300

    feat: [Create UI component PokemonOverview] - Create page components styles

--- src/components/AppOverview/styles.ts
+++ src/components/AppOverview/styles.ts
@@ -1,12 +1,40 @@
 import { css } from "iares";
 
 export const styles = () => css`
-  app-detail,
-  .wrap-ctx {
+  app-overview,
+  .wrap-ctx,
+  .wrap-image-ctx {
     display: flex;
     flex-wrap: wrap;
     width:100%;
   }
 
+  .wrap-ctx {
+    padding:2em 0
+  }
 
+  .wrap-image-ctx {
+    justify-content: center;
+    align-items:center;
+    height:500px
+  }
+
+  .wrap-image-ctx > img {
+    width:100%;
+    max-width:350px;
+  }
+
+  .wrap-tag-ctx {
+    display:grid;
+    grid-gap: 1em;
+    grid-template-columns: repeat(4, 1fr);
+    max-width: 120px
+  }
+
+  .wrap-title-ctx {
+    display: flex;
+    width:100%;
+    margin-bottom: 2em;
+    border-bottom: 1px var(--green-moss) solid
+  }
 `;
--- src/components/AppOverview/template.ts
+++ src/components/AppOverview/template.ts
@@ -2,13 +2,60 @@ import { html } from "iares";
 import { Ttemplate } from "./types";
 
 import { AppContent } from "@/components/AppContent";
+import { AppCard } from "@/components/AppCard";
+import { AppTitle } from "@/components/AppTitle";
+import { AppTag } from "@/components/AppTag";
+import { AppPokeCard } from "@/components/AppPokeCard";
 
 export const template: Ttemplate = ({ props }) => html`
   <div class="wrap-ctx">
 
     <${AppContent}>
-      <slot target="content">
-        <h1>  App Overview </h1>
+      <slot target="content" ctx="app-overview">
+        <div class="grid">
+          <div class="xxl-col-6">
+            <${AppCard}>
+              <slot target="content" ctx="app-overview">
+                <div class="wrap-image-ctx">
+                  <img src="/assets/images/pokemon-sample-2.png" />
+                </div>
+              </slot>
+            </>
+          </div>
+          <div class="xxl-col-6">
+            <${AppTitle} value="#556699 - Zaramana" height=${3} size=${1}/>
+            <div class="wrap-tag-ctx">
+              <${AppTag} value="ground"/>
+              <${AppTag} value="ground"/>
+              <${AppTag} value="ground"/>
+              <${AppTag} value="ground"/>
+            </div>
+          </div>
+        </div>
+      </slot>
+    </>
+
+    <${AppContent}>
+      <slot target="content" ctx="app-overview">
+        <div class="wrap-ctx">
+          <div class="wrap-title-ctx">
+            <${AppTitle} icon="list_alt" value="Pokemons semelhantes" height=${2} size=${2}/>
+          </div>
+          <div class="grid">
+            <div class="xxl-col-3">
+              <${AppPokeCard} image="1"/>
+            </div>
+            <div class="xxl-col-3">
+              <${AppPokeCard} image="2"/>
+            </div>
+            <div class="xxl-col-3">
+              <${AppPokeCard} image="1"/>
+            </div>
+            <div class="xxl-col-3">
+              <${AppPokeCard} image="2"/>
+            </div>
+          </div>
+        </div>
       </slot>
     </>
   </div>
